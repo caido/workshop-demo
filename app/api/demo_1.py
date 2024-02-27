9cb1e44f876ad4ece8511ea88e0f36444a151dd8
Emile Fugulin <code@efugulin.com>
2024-02-27 14:40:07 -0500

    Basic template setup

--- app/api/demo_1.py
+++ app/api/demo_1.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+
+from app.core.templates import templates
 
 router = APIRouter(prefix="/demo1", tags=["posts"])
 
 
-@router.get("/test")
-def read_root():
-    return {"hello": "world"}
+@router.get("/")
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="demo1.jinja")
+
+
+class Flag(BaseModel):
+    flag: str
+
+
+@router.post("/flag", status_code=201)
+def post_flag(flag: Flag):
+    return {"error": None}
--- app/core/app.py
+++ app/core/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.api import demo_1
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Fastapi Template")
+
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
     app.include_router(demo_1.router)
 
